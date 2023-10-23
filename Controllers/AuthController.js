86cbb4ee486dd176373976d741ecd9033a473300
Sameer <sameerkhatangalekar45@gmail.com>
2023-10-24 03:11:45 +0530

    Email service enabled

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,4 +1,6 @@
 const User = require("../Models/UserModel");
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 const {
   authSchema,
   registerSchema,
@@ -13,11 +15,9 @@ const {
   signRefreshToken,
   verifyRefreshToken,
 } = require("../Helpers/jwtHelper");
-const { sendPasswordResetEmail } = require("../Helpers/sendEmail");
 const createError = require("http-errors");
 const crypto = require("crypto");
 const joi = require("@hapi/joi");
-const ejs = require("ejs");
 
 module.exports = {
   //* Register new user
@@ -192,10 +192,186 @@ module.exports = {
       };
 
       console.log(otp);
-      res.json({
-        status: true,
-        message: `Email sent to ${user.email} successfully`,
-      });
+
+
+      try {
+        const data = await resend.emails.send({
+          from: "no-reply@transactional.thefurrstlove.com",
+          to: [user.email],
+          subject: "Password Reset",
+          html: `<!-- Change values in [brackets] in the template and pass { {variables} } with API call -->
+          <!-- Feel free to adjust it to your needs and delete all these comments-->
+          <!-- Also adapt TXT version of this email -->
+          <!DOCTYPE html>
+          <html xmlns="http://www.w3.org/1999/xhtml">
+          
+          <head>
+            <title></title>
+            <!--[if !mso]><!-- -->
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <!--<![endif]-->
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style type="text/css">
+              #outlook a {
+                padding: 0;
+              }
+          
+              .ReadMsgBody {
+                width: 100%;
+              }
+          
+              .ExternalClass {
+                width: 100%;
+              }
+          
+              .ExternalClass * {
+                line-height: 100%;
+              }
+          
+              body {
+                margin: 0;
+                padding: 0;
+                -webkit-text-size-adjust: 100%;
+                -ms-text-size-adjust: 100%;
+              }
+          
+              table,
+              td {
+                border-collapse: collapse;
+                mso-table-lspace: 0pt;
+                mso-table-rspace: 0pt;
+              }
+          
+            </style>
+            <!--[if !mso]><!-->
+            <style type="text/css">
+              @media only screen and (max-width:480px) {
+                @-ms-viewport {
+                  width: 320px;
+                }
+                @viewport {
+                  width: 320px;
+                }
+              }
+            </style>
+            <!--<![endif]-->
+            <!--[if mso]><xml>  <o:OfficeDocumentSettings>    <o:AllowPNG/>    <o:PixelsPerInch>96</o:PixelsPerInch>  </o:OfficeDocumentSettings></xml><![endif]-->
+            <!--[if lte mso 11]><style type="text/css">  .outlook-group-fix {    width:100% !important;  }</style><![endif]-->
+            <!--[if !mso]><!-->
+            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" type="text/css">
+            <style type="text/css">
+              @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
+            </style>
+            <!--<![endif]-->
+            <style type="text/css">
+              @media only screen and (max-width:595px) {
+                .container {
+                  width: 100% !important;
+                }
+                .button {
+                  display: block !important;
+                  width: auto !important;
+                }
+              }
+            </style>
+          </head>
+          
+          <body style="font-family: 'Inter', sans-serif; background: #E5E5E5;">
+            <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#F6FAFB">
+              <tbody>
+                <tr>
+                  <td valign="top" align="center">
+                    <table class="container" width="600" cellspacing="0" cellpadding="0" border="0">
+                      <tbody>
+                        <tr>
+                          <td style="padding:48px 0 30px 0; text-align: center; font-size: 14px; color: #4C83EE;">
+                            <img src="https://mailsend-email-assets.mailtrap.io/xmc1j14ju90kh7lh1ndxkmriihd1.png" style="height: 100px;" alt="" class="img-fluid">
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="main-content" style="padding: 48px 30px 40px; color: #000000;" bgcolor="#ffffff">
+                            <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                              <tbody>
+                                <tr>
+                                  <td style="padding: 0 0 24px 0; font-size: 18px; line-height: 150%; font-weight: bold; color: #000000; letter-spacing: 0.01em;">
+                                    Hello! ${user.name} Forgot your password?
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td style="padding: 0 0 10px 0; font-size: 14px; line-height: 150%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                                    We received a password reset request for your account: <span style="color: #4C83EE;">${user.email}</span>.
+                                  </td>
+                                </tr>
+                                
+                                
+                                <tr>
+                                  <td style="padding: 0 0 10px 0; font-size: 14px; line-height: 150%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                                    The password reset OTP is <strong>${otp}</strong> is only valid for the next 15 minutes.
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td style="padding: 0 0 60px 0; font-size: 14px; line-height: 150%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                                    If you didn’t request the password reset, please ignore this message.
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td style="padding: 0 0 16px;">
+                                    <span style="display: block; width: 117px; border-bottom: 1px solid #8B949F;"></span>
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td style="font-size: 14px; line-height: 170%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                                    Best regards, <br><strong>The Furrst Love</strong>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding: 24px 0 48px; font-size: 0px;">
+                            <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0">        <tr>          <td style="vertical-align:top;width:300px;">      <![endif]-->
+                            <div class="outlook-group-fix" style="padding: 0 0 20px 0; vertical-align: top; display: inline-block; text-align: center; width:100%;">
+                              <span style="padding: 0; font-size: 11px; line-height: 15px; font-weight: normal; color: #8B949F;"><strong>Dattaram Gawde - The Furrst Love</strong><br/>7 Rue Jean-Pierre Bierman , Valerie 4, 1268, Luxembourg
+                              </div>
+                            </div>
+                            <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </body>
+          </html>
+          `,
+        });
+
+        res.json({
+          status: true,
+          message: `Email sent to ${user.email} successfully`,
+        });
+
+      } catch (error) {
+        await User.findOneAndUpdate(
+          { email },
+          {
+            $set: {
+              verifyToken: undefined,
+              verifyTokenExpire: undefined,
+            },
+          }
+        );
+        return next(createError.InternalServerError(error.message));
+      }
+
+
+
+
+
       // TODO : ENABLE EMAILING SERVICE BY UNCOMMENTING BELOW CODE
       // try {
       //   await sendPasswordResetEmail({
